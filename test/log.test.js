ad9a82e1664ab5b6b3d3228db26895167a5b8ef0
Fubowen <2251754654@qq.com>
2025-06-10 13:39:58 +0800

    fix: 修复测试日志中的 shutdown 调用，确保在计时结束后执行

--- test/log.test.js
+++ test/log.test.js
@@ -1,9 +1,11 @@
 const log = require('../index')
 
 async function test() {
+    console.time()
     for (let i = 0; i < 100000; i++) {
         await log.info('这是一个测试日志,%s', i)
     }
-    log.shutdown()
+    console.timeEnd()
+    await log.shutdown()
 }
 test()

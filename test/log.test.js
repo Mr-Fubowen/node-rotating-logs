8608791b0ccc35ff3a385528caf1c1af03c124dd
Fubowen <2251754654@qq.com>
2025-06-11 21:05:26 +0800

    feat: 更新日志记录功能，增加错误处理并将版本号更新至 2.0.1

--- test/log.test.js
+++ test/log.test.js
@@ -2,8 +2,8 @@ const log = require('../index')
 
 async function test() {
     console.time()
-    for (let i = 0; i < 100000; i++) {
-        await log.info('这是一个测试日志,%s', i)
+    for (let i = 0; i < 100; i++) {
+        await log.success('这是一个测试日志,%s', i)
     }
     console.timeEnd()
     await log.shutdown()
